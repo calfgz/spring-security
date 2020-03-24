a2768b805c8d3953d6804e753b33891f557625aa
calfgz <zhong.calf@gmail.com>
2020-03-24 15:35:35 +0800

    feat: 删除无效注释

--- src/main/java/cn/calfgz/springboot/security/config/BrowserSecurityConfig.java
+++ src/main/java/cn/calfgz/springboot/security/config/BrowserSecurityConfig.java
@@ -49,27 +49,4 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-
-    /**
-     * @Configuration
-     * public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
-     *     @Override
-     *     protected void configure(HttpSecurity http) throws Exception {
-     *         http.formLogin() // 表单方式
-     *                 .and()
-     *                 .authorizeRequests() // 授权配置
-     *                 .anyRequest()  // 所有请求
-     *                 .authenticated(); // 都需要认证
-     *     }
-     *     http.formLogin() // 表单登录
-     *             // http.httpBasic() // HTTP Basic
-     *             .loginPage("/login.html")
-     *             .loginProcessingUrl("/login")
-     *             .and()
-     *             .authorizeRequests() // 授权配置
-     *             .antMatchers("/login.html").permitAll()
-     *             .anyRequest()  // 所有请求
-     *             .authenticated(); // 都需要认证
-     * }
-     */
 }
